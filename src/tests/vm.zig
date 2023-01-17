e3a269fb128540c8c811ebb93aed177f1738bdc0
David Strachan <davidstrachan92@gmail.com>
2023-01-17 21:26:51 +0000

    Operators (#11)
    * Add several monadic operators
    
    * Add tests for flip

--- src/tests/vm.zig
+++ src/tests/vm.zig
@@ -137,6 +137,33 @@ test "vm - concat" {
     try runTest(",,1", .{ .list = &[_]TestValue{.{ .int_list = &[_]TestValue{.{ .int = 1 }} }} });
 }
 
+test "vm - flip" {
+    try runTest("+(0 1;2 3)", .{
+        .list = &[_]TestValue{
+            .{ .int_list = &[_]TestValue{ .{ .int = 0 }, .{ .int = 2 } } },
+            .{ .int_list = &[_]TestValue{ .{ .int = 1 }, .{ .int = 3 } } },
+        },
+    });
+    try runTest("+(`a`b;`c`d)", .{
+        .list = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{ .{ .symbol = "a" }, .{ .symbol = "c" } } },
+            .{ .symbol_list = &[_]TestValue{ .{ .symbol = "b" }, .{ .symbol = "d" } } },
+        },
+    });
+    try runTest("+(\"ab\";\"cd\")", .{
+        .list = &[_]TestValue{
+            .{ .char_list = &[_]TestValue{ .{ .char = 'a' }, .{ .char = 'c' } } },
+            .{ .char_list = &[_]TestValue{ .{ .char = 'b' }, .{ .char = 'd' } } },
+        },
+    });
+    try runTest("+(`a;1;\"ab\";\"cd\")", .{
+        .list = &[_]TestValue{
+            .{ .list = &[_]TestValue{ .{ .symbol = "a" }, .{ .int = 1 }, .{ .char = 'a' }, .{ .char = 'c' } } },
+            .{ .list = &[_]TestValue{ .{ .symbol = "a" }, .{ .int = 1 }, .{ .char = 'b' }, .{ .char = 'd' } } },
+        },
+    });
+}
+
 test "vm - boolean addition" {
     try runTest("0b+0b", .{ .int = 0 });
     try runTest("0b+1b", .{ .int = 1 });
