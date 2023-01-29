2c97cd4608e108e8bd935a24c7c2307b7547da5c
David Strachan <davidstrachan92@gmail.com>
2023-01-29 22:49:57 +0000

    Remove auto-generated tests

--- src/main.zig
+++ src/main.zig
@@ -111,7 +111,6 @@ test {
     // _ = @import("tests/verbs/max/_imports.zig");
     // _ = @import("tests/verbs/min/_imports.zig");
     // _ = @import("tests/verbs/more/_imports.zig");
-    // _ = @import("tests/verbs/multiply/_imports.zig");
 
     _ = @import("tests/verbs/add.zig");
     _ = @import("tests/verbs/enum.zig");
