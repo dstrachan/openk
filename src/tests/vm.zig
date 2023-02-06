7d8feba7c963a95f3b45aa53e84767befecabfab
David Strachan <davidstrachan92@gmail.com>
2023-02-06 22:46:45 +0000

    Add dictionary tests

--- src/tests/vm.zig
+++ src/tests/vm.zig
@@ -28,6 +28,8 @@ pub const TestValue = union(ValueType) {
     char_list: []const Self,
     symbol_list: []const Self,
 
+    dictionary: []const Self,
+
     function: *ValueFunction,
     projection: *ValueProjection,
 };
@@ -82,6 +84,11 @@ fn compareValues(expected: TestValue, actual: ValueUnion) !void {
             try std.testing.expectEqual(expected.symbol_list.len, actual.symbol_list.len);
             for (expected.symbol_list) |value, i| try compareValues(value, actual.symbol_list[i].as);
         },
+        .dictionary => {
+            try std.testing.expectEqual(@as(usize, 2), expected.dictionary.len);
+            try compareValues(expected.dictionary[0], actual.dictionary.key.as);
+            try compareValues(expected.dictionary[1], actual.dictionary.value.as);
+        },
         .function => try std.testing.expectEqual(expected.function, actual.function),
         .projection => try std.testing.expectEqual(expected.projection, actual.projection),
     }
@@ -107,174 +114,6 @@ pub fn runTestError(input: []const u8, expected: anyerror) !void {
     };
     result.deref(std.testing.allocator);
     try std.testing.expectEqual(expected, error.no_error);
-}
-
-pub const DataType = enum {
-    boolean,
-    int,
-    float,
-};
-
-const ResultDataTypeFn = *const fn (comptime x: DataType, comptime y: DataType) DataType;
-const VerbFn = *const fn (comptime x: comptime_int, comptime y: comptime_int) comptime_float;
-const PredicateFn = *const fn (comptime x: comptime_int, comptime y: comptime_int) bool;
-
-fn getValue(comptime data_type: DataType, comptime input1: comptime_float) TestValue {
-    return switch (data_type) {
-        .boolean => .{ .int = if (input1 > 0) 1 else 0 },
-        .int => .{ .int = input1 },
-        .float => .{ .float = input1 },
-    };
-}
-
-fn getValueList(comptime data_type: DataType, comptime input1: comptime_float, comptime input2: comptime_float) TestValue {
-    return switch (data_type) {
-        .boolean => .{ .int_list = &[_]TestValue{ .{ .int = if (input1 > 0) 1 else 0 }, .{ .int = if (input2 > 0) 1 else 0 } } },
-        .int => .{ .int_list = &[_]TestValue{ .{ .int = input1 }, .{ .int = input2 } } },
-        .float => .{ .float_list = &[_]TestValue{ .{ .float = input1 }, .{ .float = input2 } } },
-    };
-}
-
-fn getValueString(comptime data_type: DataType, comptime input1: comptime_int) []const u8 {
-    return switch (data_type) {
-        .boolean => if (input1 > 0) "1b" else "0b",
-        .int => switch (input1) {
-            0 => "0",
-            1 => "1",
-            2 => "2",
-            -1 => "-1",
-            -2 => "-2",
-            else => unreachable,
-        },
-        .float => switch (input1) {
-            0 => "0f",
-            1 => "1f",
-            2 => "2f",
-            -1 => "-1f",
-            -2 => "-2f",
-            else => unreachable,
-        },
-    };
-}
-
-fn getValueStringList(comptime data_type: DataType, comptime input1: comptime_int, comptime input2: comptime_int) []const u8 {
-    return switch (data_type) {
-        .boolean => (if (input1 > 0) "1" else "0") ++ (if (input2 > 0) "1b" else "0b"),
-        .int => switch (input1) {
-            0 => "0",
-            1 => "1",
-            2 => "2",
-            -1 => "-1",
-            -2 => "-2",
-            else => unreachable,
-        } ++ " " ++ switch (input2) {
-            0 => "0",
-            1 => "1",
-            2 => "2",
-            -1 => "-1",
-            -2 => "-2",
-            else => unreachable,
-        },
-        .float => switch (input1) {
-            0 => "0",
-            1 => "1",
-            2 => "2",
-            -1 => "-1",
-            -2 => "-2",
-            else => unreachable,
-        } ++ " " ++ switch (input2) {
-            0 => "0f",
-            1 => "1f",
-            2 => "2f",
-            -1 => "-1f",
-            -2 => "-2f",
-            else => unreachable,
-        },
-    };
-}
-
-pub fn verbTest(
-    comptime data_types: []const DataType,
-    comptime inputs: []const comptime_int,
-    comptime exclude_predicate: ?PredicateFn,
-    comptime result_data_type_fn: ResultDataTypeFn,
-    comptime verb_fn: VerbFn,
-    comptime verb_str: []const u8,
-) !void {
-    _ = data_types;
-    _ = inputs;
-    _ = exclude_predicate;
-    _ = result_data_type_fn;
-    _ = verb_fn;
-    _ = verb_str;
-    // @setEvalBranchQuota(20000);
-    // inline for (.{ .atom, .list }) |list_type_x| {
-    //     inline for (.{ .atom, .list }) |list_type_y| {
-    //         inline for (data_types) |data_type_x| {
-    //             inline for (data_types) |data_type_y| {
-    //                 inline for (inputs) |input_x1| {
-    //                     inline for (inputs) |input_x2| {
-    //                         inline for (inputs) |input_y1| {
-    //                             inline for (inputs) |input_y2| {
-    //                                 if (data_type_x == .boolean or data_type_y == .boolean) {
-    //                                     if (input_x1 == -1 or input_x2 == -1 or input_y1 == -1 or input_y2 == -1) continue;
-    //                                 }
-    //                                 comptime if (exclude_predicate != null and switch (list_type_x) {
-    //                                     .atom => switch (list_type_y) {
-    //                                         .atom => exclude_predicate.?(input_x1, input_y1),
-    //                                         .list => exclude_predicate.?(input_x1, input_y1) or exclude_predicate.?(input_x1, input_y2),
-    //                                         else => unreachable,
-    //                                     },
-    //                                     .list => switch (list_type_y) {
-    //                                         .atom => exclude_predicate.?(input_x1, input_y1) or exclude_predicate.?(input_x2, input_y1),
-    //                                         .list => exclude_predicate.?(input_x1, input_y1) or exclude_predicate.?(input_x2, input_y2),
-    //                                         else => unreachable,
-    //                                     },
-    //                                     else => unreachable,
-    //                                 }) {
-    //                                     continue;
-    //                                 };
-
-    //                                 const expected_data_type = comptime result_data_type_fn(data_type_x, data_type_y);
-    //                                 const expected = switch (list_type_x) {
-    //                                     .atom => switch (list_type_y) {
-    //                                         .atom => getValue(expected_data_type, verb_fn(input_x1, input_y1)),
-    //                                         .list => getValueList(expected_data_type, verb_fn(input_x1, input_y1), verb_fn(input_x1, input_y2)),
-    //                                         else => unreachable,
-    //                                     },
-    //                                     .list => switch (list_type_y) {
-    //                                         .atom => getValueList(expected_data_type, verb_fn(input_x1, input_y1), verb_fn(input_x2, input_y1)),
-    //                                         .list => getValueList(expected_data_type, verb_fn(input_x1, input_y1), verb_fn(input_x2, input_y2)),
-    //                                         else => unreachable,
-    //                                     },
-    //                                     else => unreachable,
-    //                                 };
-
-    //                                 const x_string = comptime switch (list_type_x) {
-    //                                     .atom => getValueString(data_type_x, input_x1),
-    //                                     .list => getValueStringList(data_type_x, input_x1, input_x2),
-    //                                     else => unreachable,
-    //                                 };
-    //                                 const y_string = comptime switch (list_type_y) {
-    //                                     .atom => getValueString(data_type_y, input_y1),
-    //                                     .list => getValueStringList(data_type_y, input_y1, input_y2),
-    //                                     else => unreachable,
-    //                                 };
-    //                                 const input = x_string ++ verb_str ++ y_string;
-
-    //                                 runTest(input, expected) catch |err| {
-    //                                     std.debug.print("input = {s}\n", .{input});
-    //                                     std.debug.print("expected = {}\n", .{expected});
-    //                                     return err;
-    //                                 };
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
 }
 
 test "top-level script returns result" {
@@ -403,4 +242,53 @@ test "function errors don't leak memory" {
 test "projections don't leak memory" {
     try runTest("{[x;y]x-y}[1];", TestValue.nil);
     try runTest("a:{[x;y]x-y}[1];", TestValue.nil);
+}
+
+test "dictionaries" {
+    try runTest("()!()", .{
+        .dictionary = &[_]TestValue{
+            .{ .list = &[_]TestValue{} },
+            .{ .list = &[_]TestValue{} },
+        },
+    });
+    try runTest("`a`b`c!1 2 3", .{
+        .dictionary = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{
+                .{ .symbol = "a" },
+                .{ .symbol = "b" },
+                .{ .symbol = "c" },
+            } },
+            .{ .int_list = &[_]TestValue{
+                .{ .int = 1 },
+                .{ .int = 2 },
+                .{ .int = 3 },
+            } },
+        },
+    });
+    try runTest("(`a`b`c!1 2 3)`a", .{ .int = 1 });
+    try runTest("(`a`b`c!1 2 3)`d", .{ .int = Value.null_int });
+    try runTest("(`a`b`c!1 2 3)`a`b", .{
+        .int_list = &[_]TestValue{
+            .{ .int = 1 },
+            .{ .int = 2 },
+        },
+    });
+    try runTest("(`a`b`c!1 2 3)`a`d", .{
+        .int_list = &[_]TestValue{
+            .{ .int = 1 },
+            .{ .int = Value.null_int },
+        },
+    });
+    try runTest("(`a`b`c!1 2 3)`d`a", .{
+        .int_list = &[_]TestValue{
+            .{ .int = Value.null_int },
+            .{ .int = 1 },
+        },
+    });
+    try runTest("(`a`b`c!1 2 3)`d`e", .{
+        .int_list = &[_]TestValue{
+            .{ .int = Value.null_int },
+            .{ .int = Value.null_int },
+        },
+    });
 }
