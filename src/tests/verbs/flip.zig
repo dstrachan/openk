aa77029133ecc306c975a4376ba10c3cb2e6021c
David Strachan <davidstrachan92@gmail.com>
2023-02-28 20:59:40 +0000

    Flip tests (#40)
    * Add extra flip tests
    
    * Fix flip tests

--- src/tests/verbs/flip.zig
+++ src/tests/verbs/flip.zig
@@ -5,7 +5,40 @@ const TestValue = vm_mod.TestValue;
 
 const FlipError = @import("../../verbs/flip.zig").FlipError;
 
-test "flip" {
+test "flip boolean" {
+    try runTestError("+0b", FlipError.invalid_type);
+    try runTestError("+`boolean$()", FlipError.invalid_type);
+    try runTestError("+01b", FlipError.invalid_type);
+}
+
+test "flip int" {
+    try runTestError("+0", FlipError.invalid_type);
+    try runTestError("+`int$()", FlipError.invalid_type);
+    try runTestError("+0 1 0N 0W -0W", FlipError.invalid_type);
+}
+
+test "flip float" {
+    try runTestError("+0f", FlipError.invalid_type);
+    try runTestError("+`float$()", FlipError.invalid_type);
+    try runTestError("+0 1 0n 0w -0w", FlipError.invalid_type);
+}
+
+test "flip char" {
+    try runTestError("+\"a\"", FlipError.invalid_type);
+    try runTestError("+\"\"", FlipError.invalid_type);
+    try runTestError("+\"abcde\"", FlipError.invalid_type);
+}
+
+test "flip symbol" {
+    try runTestError("+`symbol", FlipError.invalid_type);
+    try runTestError("+`$()", FlipError.invalid_type);
+    try runTestError("+`a`b`c`d`e", FlipError.invalid_type);
+}
+
+test "flip list" {
+    try runTest("+()", .{ .list = &[_]TestValue{} });
+    try runTestError("+(0b;1;2f)", FlipError.length_mismatch);
+
     try runTest("+(0 1;2 3)", .{
         .list = &[_]TestValue{
             .{ .int_list = &[_]TestValue{
@@ -92,11 +125,52 @@ test "flip" {
         },
     });
 
-    try runTestError("+1", FlipError.invalid_type);
-    try runTestError("+(1;2f)", FlipError.invalid_type);
+    try runTestError("+(1;2f)", FlipError.length_mismatch);
     try runTestError("+(1;2 3;4 5 6)", FlipError.length_mismatch);
     try runTestError("+(1;2 3;())", FlipError.length_mismatch);
+}
 
+test "flip dictionary" {
+    try runTest("+`a`b!(();())", .{
+        .table = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{
+                .{ .symbol = "a" },
+                .{ .symbol = "b" },
+            } },
+            .{ .list = &[_]TestValue{
+                .{ .list = &[_]TestValue{} },
+                .{ .list = &[_]TestValue{} },
+            } },
+        },
+    });
+    try runTest("+`a`b!(`int$();`float$())", .{
+        .table = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{
+                .{ .symbol = "a" },
+                .{ .symbol = "b" },
+            } },
+            .{ .list = &[_]TestValue{
+                .{ .int_list = &[_]TestValue{} },
+                .{ .float_list = &[_]TestValue{} },
+            } },
+        },
+    });
+    try runTest("+`a`b!(,1;,2)", .{
+        .table = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{
+                .{ .symbol = "a" },
+                .{ .symbol = "b" },
+            } },
+            .{ .list = &[_]TestValue{
+                .{ .int_list = &[_]TestValue{
+                    .{ .int = 1 },
+                } },
+                .{ .int_list = &[_]TestValue{
+                    .{ .int = 2 },
+                } },
+            } },
+        },
+    });
     try runTest("+`a`b!(1 1;2 2)", .{
         .table = &[_]TestValue{
             .{ .symbol_list = &[_]TestValue{
@@ -135,9 +209,35 @@ test "flip" {
     });
     try runTestError("+1 2!1 2", FlipError.invalid_column_type);
     try runTestError("+`a`b!1 2", FlipError.invalid_value_type);
+}
 
-    try runTest("+`a`b!(,1;,2)", .{
-        .table = &[_]TestValue{
+test "flip table" {
+    try runTest("++`a`b!(();())", .{
+        .dictionary = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{
+                .{ .symbol = "a" },
+                .{ .symbol = "b" },
+            } },
+            .{ .list = &[_]TestValue{
+                .{ .list = &[_]TestValue{} },
+                .{ .list = &[_]TestValue{} },
+            } },
+        },
+    });
+    try runTest("++`a`b!(`int$();`float$())", .{
+        .dictionary = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{
+                .{ .symbol = "a" },
+                .{ .symbol = "b" },
+            } },
+            .{ .list = &[_]TestValue{
+                .{ .int_list = &[_]TestValue{} },
+                .{ .float_list = &[_]TestValue{} },
+            } },
+        },
+    });
+    try runTest("++`a`b!(,1;,2)", .{
+        .dictionary = &[_]TestValue{
             .{ .symbol_list = &[_]TestValue{
                 .{ .symbol = "a" },
                 .{ .symbol = "b" },
@@ -152,8 +252,7 @@ test "flip" {
             } },
         },
     });
-
-    try runTest("++`a`b!(,1;,2)", .{
+    try runTest("++`a`b!(1 1;2 2)", .{
         .dictionary = &[_]TestValue{
             .{ .symbol_list = &[_]TestValue{
                 .{ .symbol = "a" },
@@ -162,8 +261,28 @@ test "flip" {
             .{ .list = &[_]TestValue{
                 .{ .int_list = &[_]TestValue{
                     .{ .int = 1 },
+                    .{ .int = 1 },
                 } },
                 .{ .int_list = &[_]TestValue{
+                    .{ .int = 2 },
+                    .{ .int = 2 },
+                } },
+            } },
+        },
+    });
+    try runTest("++`a`b!(1;2 2)", .{
+        .dictionary = &[_]TestValue{
+            .{ .symbol_list = &[_]TestValue{
+                .{ .symbol = "a" },
+                .{ .symbol = "b" },
+            } },
+            .{ .list = &[_]TestValue{
+                .{ .int_list = &[_]TestValue{
+                    .{ .int = 1 },
+                    .{ .int = 1 },
+                } },
+                .{ .int_list = &[_]TestValue{
+                    .{ .int = 2 },
                     .{ .int = 2 },
                 } },
             } },
