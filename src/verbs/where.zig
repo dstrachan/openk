c9cc8999b20490609456d9a22081b4c7b9752cd5
David Strachan <davidstrachan92@gmail.com>
2023-03-01 20:32:46 +0000

    Added where implementation

--- src/verbs/where.zig
+++ src/verbs/where.zig
@@ -10,21 +10,26 @@ const ValueType = value_mod.ValueType;
 const vm_mod = @import("../vm.zig");
 const VM = vm_mod.VM;
 
+const index = @import("index.zig").index;
+
 pub const WhereError = error{
     invalid_type,
     negative_number,
+    list_limit,
 };
 
 fn runtimeError(comptime err: WhereError) WhereError!*Value {
     switch (err) {
         WhereError.invalid_type => print("Invalid type.\n", .{}),
         WhereError.negative_number => print("Cannot generate list from negative input.\n", .{}),
+        WhereError.list_limit => print("Cannot generate a list longer than 10,085,382,816 elements.\n", .{}),
     }
     return err;
 }
 
 pub fn where(vm: *VM, x: *Value) WhereError!*Value {
     return switch (x.as) {
+        .list => |list_x| if (list_x.len == 0) vm.initValue(.{ .int_list = &[_]*Value{} }) else runtimeError(WhereError.invalid_type),
         .boolean_list => |bool_list_x| blk: {
             var list = std.ArrayList(*Value).init(vm.allocator);
             for (bool_list_x, 0..) |value, i| {
@@ -33,14 +38,28 @@ pub fn where(vm: *VM, x: *Value) WhereError!*Value {
             break :blk vm.initValue(.{ .int_list = list.toOwnedSlice() catch std.debug.panic("Failed to create list.", .{}) });
         },
         .int_list => |int_list_x| blk: {
-            var list = std.ArrayList(*Value).init(vm.allocator);
-            errdefer list.deinit();
-            for (int_list_x, 0..) |value, i| {
-                errdefer for (list.items) |v| v.deref(vm.allocator);
+            var len: i64 = 0;
+            for (int_list_x) |value| {
                 if (value.as.int < 0) return runtimeError(WhereError.negative_number);
-                if (value.as.int > 0) list.appendNTimes(vm.initValue(.{ .int = @intCast(i64, i) }), @intCast(usize, value.as.int)) catch std.debug.panic("Failed to append item.", .{});
+                len = std.math.add(i64, len, value.as.int) catch return runtimeError(WhereError.list_limit);
             }
-            break :blk vm.initValue(.{ .int_list = list.toOwnedSlice() catch std.debug.panic("Failed to create list.", .{}) });
+
+            const list = vm.allocator.alloc(*Value, @intCast(usize, len)) catch return runtimeError(WhereError.list_limit);
+            var i: usize = 0;
+            for (int_list_x, 0..) |value, j| {
+                if (value.as.int > 0) {
+                    const v = vm.initValueWithRefCount(0, .{ .int = @intCast(i64, j) });
+                    for (i..i + @intCast(usize, value.as.int)) |idx| list[idx] = v.ref();
+                    i += @intCast(usize, value.as.int);
+                }
+            }
+
+            break :blk vm.initValue(.{ .int_list = list });
+        },
+        .dictionary => |dict_x| blk: {
+            const indices = try where(vm, dict_x.value);
+            defer indices.deref(vm.allocator);
+            break :blk index(vm, dict_x.key, indices) catch runtimeError(WhereError.invalid_type);
         },
         else => runtimeError(WhereError.invalid_type),
     };
