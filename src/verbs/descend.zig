19ab148b0993bfb3fee643cad7dbe50825a0e50e
David Strachan <davidstrachan92@gmail.com>
2023-03-18 10:13:34 +0000

    Use MultiArrayList for descend

--- src/verbs/descend.zig
+++ src/verbs/descend.zig
@@ -23,48 +23,47 @@ fn runtimeError(comptime err: DescendError) DescendError!*Value {
     return err;
 }
 
-fn desc(context: void, a: Pair, b: Pair) bool {
-    _ = context;
-    return switch (a.value.as) {
-        .boolean => |bool_a| switch (b.value.as) {
+fn desc(a: *Value, b: *Value) bool {
+    return switch (a.as) {
+        .boolean => |bool_a| switch (b.as) {
             .boolean => |bool_b| @boolToInt(bool_a) > @boolToInt(bool_b),
             else => false,
         },
-        .int => |int_a| switch (b.value.as) {
+        .int => |int_a| switch (b.as) {
             .boolean => true,
             .int => |int_b| int_a > int_b,
             else => false,
         },
-        .float => |float_a| switch (b.value.as) {
+        .float => |float_a| switch (b.as) {
             .boolean, .int => true,
             .float => |float_b| float_a > float_b,
             else => false,
         },
-        .char => |char_a| switch (b.value.as) {
+        .char => |char_a| switch (b.as) {
             .boolean, .int, .float => true,
             .char => |char_b| char_a > char_b,
             else => false,
         },
-        .symbol => |symbol_a| switch (b.value.as) {
+        .symbol => |symbol_a| switch (b.as) {
             .boolean, .int, .float, .char => true,
             .symbol => |symbol_b| std.mem.order(u8, symbol_a, symbol_b) == .gt,
             else => false,
         },
-        .list, .boolean_list, .int_list, .float_list, .char_list, .symbol_list => |list_a| switch (b.value.as) {
+        .list, .boolean_list, .int_list, .float_list, .char_list, .symbol_list => |list_a| switch (b.as) {
             .list, .boolean_list, .int_list, .float_list, .char_list, .symbol_list => |list_b| blk: {
-                if (@as(ValueType, a.value.as) != b.value.as) break :blk @enumToInt(a.value.as) > @enumToInt(b.value.as);
+                if (@as(ValueType, a.as) != b.as) break :blk @enumToInt(a.as) > @enumToInt(b.as);
 
                 const len = std.math.min(list_a.len, list_b.len);
 
                 var i: usize = 0;
                 while (i < len) : (i += 1) {
                     if (list_a[i].eql(list_b[i])) continue;
-                    break :blk desc({}, .{ .value = list_a[i] }, .{ .value = list_b[i] });
+                    break :blk desc(list_a[i], list_b[i]);
                 }
 
                 break :blk list_a.len > list_b.len;
             },
-            else => @enumToInt(a.value.as) > @enumToInt(b.value.as),
+            else => @enumToInt(a.as) > @enumToInt(b.as),
         },
         else => unreachable,
     };
@@ -75,69 +74,74 @@ const Pair = struct {
     index: *Value = undefined,
 };
 
+const PairContext = struct {
+    values: []*Value,
+
+    pub fn lessThan(ctx: @This(), a_index: usize, b_index: usize) bool {
+        return desc(ctx.values[a_index], ctx.values[b_index]);
+    }
+};
+
 pub fn descend(vm: *VM, x: *Value) DescendError!*Value {
     return switch (x.as) {
         .list, .boolean_list, .int_list, .float_list, .char_list, .symbol_list => |list_x| blk: {
             if (list_x.len == 0) break :blk vm.initValue(.{ .int_list = &.{} });
 
-            const pairs = vm.allocator.alloc(Pair, list_x.len) catch std.debug.panic("Failed to create list.", .{});
-            defer vm.allocator.free(pairs);
+            var list = std.MultiArrayList(Pair){};
+            defer list.deinit(vm.allocator);
+
+            list.ensureTotalCapacity(vm.allocator, list_x.len) catch std.debug.panic("Failed to create list.", .{});
             for (list_x, 0..) |v, i| {
-                pairs[i] = .{
+                list.appendAssumeCapacity(.{
                     .value = v,
                     .index = vm.initValue(.{ .int = @intCast(i64, i) }),
-                };
+                });
             }
-            std.sort.sort(Pair, pairs, {}, desc);
-            const list = vm.allocator.alloc(*Value, pairs.len) catch std.debug.panic("Failed to create list.", .{});
-            for (pairs, 0..) |p, i| {
-                list[i] = p.index;
-            }
-            break :blk vm.initValue(.{ .int_list = list });
+            list.sort(PairContext{ .values = list.items(.value) });
+            break :blk vm.initValue(.{ .int_list = list.toOwnedSlice().items(.index) });
         },
         .dictionary => |dict_x| blk: {
             if (dict_x.keys.asList().len == 0) break :blk dict_x.keys.ref();
 
-            const pairs = vm.allocator.alloc(Pair, dict_x.keys.asList().len) catch std.debug.panic("Failed to create list.", .{});
-            defer vm.allocator.free(pairs);
-            for (dict_x.values.asList(), 0..) |v, i| {
-                pairs[i] = .{
+            var list = std.MultiArrayList(Pair){};
+            defer list.deinit(vm.allocator);
+
+            list.ensureTotalCapacity(vm.allocator, dict_x.keys.asList().len) catch std.debug.panic("Failed to create list.", .{});
+            for (dict_x.keys.asList(), dict_x.values.asList()) |k, v| {
+                list.appendAssumeCapacity(.{
                     .value = v,
-                    .index = dict_x.keys.asList()[i].ref(),
-                };
+                    .index = k.ref(),
+                });
             }
-            std.sort.sort(Pair, pairs, {}, desc);
-            const list = vm.allocator.alloc(*Value, pairs.len) catch std.debug.panic("Failed to create list.", .{});
-            for (pairs, 0..) |p, i| {
-                list[i] = p.index;
-            }
-            break :blk vm.initList(list, dict_x.keys.as);
+            list.sort(PairContext{ .values = list.items(.value) });
+            break :blk vm.initList(list.toOwnedSlice().items(.index), dict_x.keys.as);
         },
         .table => |table_x| blk: {
             const len = table_x.values.as.list[0].asList().len;
             if (len == 0) break :blk vm.initValue(.{ .int_list = &.{} });
 
-            const pairs = vm.allocator.alloc(Pair, len) catch std.debug.panic("Failed to create list.", .{});
-            defer vm.allocator.free(pairs);
+            var list = std.MultiArrayList(Pair){};
+            defer list.deinit(vm.allocator);
+
+            list.ensureTotalCapacity(vm.allocator, len) catch std.debug.panic("Failed to create list.", .{});
             var i: usize = 0;
             while (i < len) : (i += 1) {
-                const list = vm.allocator.alloc(*Value, table_x.columns.as.symbol_list.len) catch std.debug.panic("Failed to create list.", .{});
-                for (list, 0..) |*v, j| {
+                const temp_list = vm.allocator.alloc(*Value, table_x.columns.as.symbol_list.len) catch std.debug.panic("Failed to create list.", .{});
+                for (temp_list, 0..) |*v, j| {
                     v.* = table_x.values.as.list[j].asList()[i].ref();
                 }
-                const value = vm.initValue(.{ .list = list });
-                pairs[i] = .{
+                const value = vm.initValue(.{ .list = temp_list });
+                list.appendAssumeCapacity(.{
                     .value = value,
                     .index = vm.initValue(.{ .int = @intCast(i64, i) }),
-                };
+                });
             }
-            std.sort.sort(Pair, pairs, {}, desc);
-            const list = vm.allocator.alloc(*Value, len) catch std.debug.panic("Failed to create list.", .{});
-            for (pairs, 0..) |p, j| {
-                p.value.deref(vm.allocator);
-                list[j] = p.index;
+            list.sort(PairContext{ .values = list.items(.value) });
+            const slice = list.toOwnedSlice();
+            for (slice.items(.value)) |v| {
+                v.deref(vm.allocator);
             }
-            break :blk vm.initValue(.{ .int_list = list });
+            break :blk vm.initValue(.{ .int_list = slice.items(.index) });
         },
         else => runtimeError(DescendError.invalid_type),
     };
