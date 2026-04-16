0ae2627ed5c830c713c73563527250466cbb531b
David Strachan <david@kdblint.com>
2026-04-16 20:00:45 +0100

    Implement count

--- src/k/unary_primitives/count.zig
+++ src/k/unary_primitives/count.zig
@@ -3,5 +3,18 @@ const Vm = k.Vm;
 const Value = k.Value;
 
 pub fn count(vm: *Vm, x: *Value) !*Value {
-    return vm.runtimeError("nyi: {s}[{t}]", .{ @src().fn_name, x.as });
+    return .long(vm.gpa, switch (x.as) {
+        inline .list,
+        .boolean_list,
+        .byte_list,
+        .short_list,
+        .int_list,
+        .long_list,
+        .real_list,
+        .float_list,
+        .char_list,
+        .symbol_list,
+        => |list| @intCast(list.len),
+        else => 1,
+    });
 }
