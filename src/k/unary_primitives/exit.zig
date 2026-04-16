e0e9e8266e60944e3cffa72702f85d35fb3c372b
David Strachan <david@kdblint.com>
2026-04-16 22:04:54 +0100

    Ensure exit code wraps

--- src/k/unary_primitives/exit.zig
+++ src/k/unary_primitives/exit.zig
@@ -4,10 +4,10 @@ const k = @import("../../root.zig");
 const Vm = k.Vm;
 const Value = k.Value;
 
-pub fn exit(vm: *Vm, x: *Value) !*Value {
+pub fn exit(vm: *Vm, x: *Value) !noreturn {
     const status: u8 = switch (x.as) {
         .boolean => |v| @intFromBool(v),
-        .byte, .short, .int, .long, .char => |v| @intCast(v),
+        .byte, .short, .int, .long, .char => |v| @intCast(v & 0xff),
         else => return vm.runtimeError("type", .{}),
     };
     std.process.exit(status);
