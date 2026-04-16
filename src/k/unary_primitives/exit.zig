07b05aed1ab10c8fff3a11fb91853461390ec3c7
David Strachan <david@kdblint.com>
2026-04-16 21:58:44 +0100

    Implement exit

--- src/k/unary_primitives/exit.zig
+++ src/k/unary_primitives/exit.zig
@@ -1,7 +1,14 @@
+const std = @import("std");
+
 const k = @import("../../root.zig");
 const Vm = k.Vm;
 const Value = k.Value;
 
 pub fn exit(vm: *Vm, x: *Value) !*Value {
-    return vm.runtimeError("nyi: {s}[{t}]", .{ @src().fn_name, x.as });
+    const status: u8 = switch (x.as) {
+        .boolean => |v| @intFromBool(v),
+        .byte, .short, .int, .long, .char => |v| @intCast(v),
+        else => return vm.runtimeError("type", .{}),
+    };
+    std.process.exit(status);
 }
